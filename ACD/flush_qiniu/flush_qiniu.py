a64638029202d913286516c9deb8078e82d7f5c4
unknown <miles.peng@SHD008.aspectgaming.com>
2016-06-16 12:07:01 +0800

    add new func by miles

--- ACD/flush_qiniu/flush_qiniu.py
+++ ACD/flush_qiniu/flush_qiniu.py
@@ -1,11 +1,11 @@
 #! /usr/bin/python
-
 import requests
 import json
+import ConfigParser
+import sys
 from qiniu import  Auth, put_file,etag
 
-access_key= "pL738cMCowEfEIliey3iR6kl9bP1rdpk5dGrZfoQ"
-secret_key="bCMffocLKb3Q_L9LkJGNtO1qUpN0tX1RSVrT7MmP"
+
 
 def check(url,values):
     headers = {'content-type': 'application/json'}
@@ -20,26 +20,33 @@ def qiniu_init(access_key,secret_key):
     q=Auth(access_key,secret_key)
     return q
 
-def put_file(d_file,q):
+def put_file(list_files,bucket_name,q):
+    for file_ in list_files:
+        token=q.upload_token(bucket_name,file_,3600)
+        ret,info=put_file(token,file_,file_)
+        print(info)
+        assert ret['key'] == file_
+        assert ret['hash'] == etag(file_)
+    return True
 
-    bucket_name = d_file.get("bucket_name")    #要上传的空间
-    key = d_file.get("dest_filename")              #上传到七牛后保存的文件名
-    #policy={    'callbackUrl':'http://your.domain.com/callback.php',
-    #'callbackBody':'filename=$(fname)&filesize=$(fsize)'
-    # }
+def get_filename(path):
+    list_files=[]
 
-    #token = q.upload_token(bucket_name, key, 3600,policy)       #生成上传 Token，可以指定过期时间等
-    token = q.upload_token(bucket_name, key, 3600)
-    localfile = d_file.get("sour_filename")     #要上传文件的本地路径
-
-    ret, info = put_file(token, key, localfile)
-    print(info)
-    assert ret['key'] == key
-    assert ret['hash'] == etag(localfile)       #  上传&回调
-
+    return list_files
 
 def main():
+    cf=ConfigParser.SafeConfigParser()
+    cf.read("qiniu.ini")
+    access_key=cf.get("common","access_key")
+    secret_key=cf.get("common","secret_key")
+    put_dir=sys.argv[1]
+    list_files=get_filename(put_dir)
+    section=sys.argv[2]
+    bucket_name=cf.get(section,"bucket_name")
     q=qiniu_init(access_key,secret_key)
+
+    if put_file(list_files,bucket_name,q):
+        print("Update %s success !"%put_dir)
 
 
 
