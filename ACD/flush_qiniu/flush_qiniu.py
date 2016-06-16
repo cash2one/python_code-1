11b3af35d5d7ff0a3bf4dc179f6249659d0ff7ac
unknown <miles.peng@SHD008.aspectgaming.com>
2016-06-16 11:27:54 +0800

    Update old code again

--- ACD/flush_qiniu/flush_qiniu.py
+++ ACD/flush_qiniu/flush_qiniu.py
@@ -2,43 +2,46 @@
 
 import requests
 import json
-from qiniu import  Auth
+from qiniu import  Auth, put_file,etag
+
+access_key= "pL738cMCowEfEIliey3iR6kl9bP1rdpk5dGrZfoQ"
+secret_key="bCMffocLKb3Q_L9LkJGNtO1qUpN0tX1RSVrT7MmP"
 
 def check(url,values):
     headers = {'content-type': 'application/json'}
 
-    # headers={
-    #     "X-Requested-With": "XMLHttpRequest",
-    #     "Accept": "application/json, text/javascript, */*; q=0.01",
-    #     "Access-Control-Allow-Origin": "*",
-    #     "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.97 Safari/537.36",
-    #     "Connection": "keep-alive",
-    #     "Accept-Encoding": "gzip, deflate",
-    #     "Content-Length": "65",
-    #     "Content-Type": "application/json;charset=utf-8"
-    # }
-    #pdb.set_trace()
     s = requests.Session()
     r=s.post(url,data = json.dumps(values),headers=headers)
-    #r=requests.post(url,data=json.dumps(values),headers=headers)
 
     the_page = r.content
     return the_page
 
-def main():
-    url ="http:://fusion.qiniuapi.com/refresh"
-    access_key= "pL738cMCowEfEIliey3iR6kl9bP1rdpk5dGrZfoQ"
-    secret_key="bCMffocLKb3Q_L9LkJGNtO1qUpN0tX1RSVrT7MmP"
+def qiniu_init(access_key,secret_key):
     q=Auth(access_key,secret_key)
+    return q
 
-    #//fusion.qiniuapi.com/refresh"
-    values={
-    "accound":"yuantest003@sina.com",
-    "password":"12345678"
-    }
-    page_info=check(url,values)
-   # pdb.set_trace()
-    print page_info
+def put_file(d_file,q):
+
+    bucket_name = d_file.get("bucket_name")    #要上传的空间
+    key = d_file.get("dest_filename")              #上传到七牛后保存的文件名
+    #policy={    'callbackUrl':'http://your.domain.com/callback.php',
+    #'callbackBody':'filename=$(fname)&filesize=$(fsize)'
+    # }
+
+    #token = q.upload_token(bucket_name, key, 3600,policy)       #生成上传 Token，可以指定过期时间等
+    token = q.upload_token(bucket_name, key, 3600)
+    localfile = d_file.get("sour_filename")     #要上传文件的本地路径
+
+    ret, info = put_file(token, key, localfile)
+    print(info)
+    assert ret['key'] == key
+    assert ret['hash'] == etag(localfile)       #  上传&回调
+
+
+def main():
+    q=qiniu_init(access_key,secret_key)
+
+
 
 if __name__ == '__main__':
     main()
