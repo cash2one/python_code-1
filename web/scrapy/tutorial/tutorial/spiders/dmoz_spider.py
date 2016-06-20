97f21034354d149c947ca16401e2c3fe0075a37c
unknown <miles.peng@SHD008.aspectgaming.com>
2016-06-20 18:11:58 +0800

    scrapy 手册示例01

--- web/scrapy/tutorial/tutorial/spiders/dmoz_spider.py
+++ web/scrapy/tutorial/tutorial/spiders/dmoz_spider.py
@@ -7,10 +7,17 @@ class DmozSpider(scrapy.spiders.Spider):
     name="dmoz"
     allowed_domains=["dmoz.org"]
     start_urls=[
-        "http://www.dmoz.org/Computer/Programing/Languages/Python/Books/",
-        "http://www.dmoz.org/Computer/Programing/Languages/Python/Resources/"
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
     ]
+    # def parse(self, response):
+    #     filename=response.url.split("/")[-2]
+    #     with open(filename,"wb") as f:
+    #         f.write(response.body)
+
     def parse(self, response):
-        filename=response.url.split("/")[-2]
-        with open(filename,"wb") as f:
-            f.write(response.body)
+        for sel in response.xpath("//ul/li"):
+            title=sel.xpath("a/text()").extract()
+            link=sel.xpath("a/@href").extract()
+            desc=sel.xpath("text()").extract()
+            print title,link,desc
