a0e9c773e084e1562f538915b7a875a1b90d37ab
unknown <miles.peng@SHD008.aspectgaming.com>
2016-07-04 15:43:36 +0800

    check docker if not runing start it

--- ACD/check_docker/check_docker.py
+++ ACD/check_docker/check_docker.py
@@ -81,6 +81,8 @@ def check_docker(docker_list):
 def start_docer(name):
     cli = docker.Client(base_url='unix://var/run/docker.sock')
     cli.start(container=name)
+    msg="Docker named %s start"%name
+    logMsg("start",msg,2)
     return True
 
 def main():
